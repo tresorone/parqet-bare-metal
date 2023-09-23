b687ac97b68729d7c00cb0e24d4591bacfda6943
Julian <juliandomke@outlook.de>
2023-09-23 11:41:34 +0200

    add test

---
+++ __test__/index.spec.ts
@@ -0,0 +1,14 @@
+import test from 'ava'
+
+import { getXirr } from '../index'
+
+test('xirr', (t) => {
+  const today = new Date()
+  const years = Array.from({ length: 6 }).map((_, idx) => today.getFullYear() - idx)
+  const xirr = getXirr(
+    [-1000, 100, 100, 100, 100, 1000],
+    years.map((n) => new Date(n).getTime()),
+  )
+
+  t.is(xirr, 0)
+})
